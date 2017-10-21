66d0c2cc543fbca4b4cb39f73b73fca1c13fc782
Vanessa Montero <vanessamontero@Vanessas-MacBook-Pro.local>
2017-10-21 16:35:53 -0700

    update

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '	MoxzqyA8Cn0vyK0jr9ZKOfF95',
+  consumer_secret: 'pdJyfU4teDAuzPH6DCWm6ElS6VxuPgCXLB91gRwCnvoxomV7Sp',
+  access_token_key: '908142528620138496-Q0IMy2gYfZ3VcQc2M4cO4Z5SfZslJ97',
+  access_token_secret: 'opOi03M8qJf3uBE0gJIfJTeJZLdxmU5tNvmEfYSUGvprH',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,1 @@
+var twitterKeys = require(keys.js)
